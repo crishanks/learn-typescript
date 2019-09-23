c7400c468974e9eef3b5782afda2187a379ae528
github username <crishanks12@gmail.com>
2019-09-23 10:30:43 -0700

    practice with bank account example

--- app.js
+++ app.js
@@ -89,3 +89,16 @@ var canAlsoBeNull;
 canAlsoBeNull = null;
 var canThisBeAny = null;
 canThisBeAny = 12;
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: "Cris",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
--- app.ts
+++ app.ts
@@ -111,3 +111,24 @@ let canAlsoBeNull;
 canAlsoBeNull = null;
 let canThisBeAny: number | null = null;
 canThisBeAny = 12;
+
+
+//practice
+type BankAccount = { money: number, deposit: (val: number) => void }
+
+let bankAccount: BankAccount = {
+    money: 2000, 
+    deposit(value: number): void {
+        this.money += value;
+    }
+}
+
+let myself: { name: string, bankAccount: BankAccount, hobbies: string[] } = {
+    name: "Cris",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
