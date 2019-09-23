70eb80bfc798054b7b9effe3187b7543171ec962
github username <crishanks12@gmail.com>
2019-09-23 10:07:10 -0700

    types

--- app.ts
+++ app.ts
@@ -78,3 +78,13 @@ let complex: {data: number[], output: (all: boolean) => number[]} = {
         return this.data;
     }
 };
+
+//types
+type Complex = {data: number[], output: (all: boolean) => number[]}
+
+let complex2: Complex = {
+    data: [100, 3.99, 10],
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+};
